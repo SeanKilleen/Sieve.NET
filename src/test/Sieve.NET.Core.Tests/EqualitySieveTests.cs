9e8ed29a6d4ef17a584b2321c96ab6f229abcc96
Sean Killeen <SeanKilleen@gmail.com>
2014-05-26 13:29:49 -0400

    EqualitySieve for single values.
    This should resolve #2, #17, and #18. I need to get better about
    breaking up my commits.

--- src/test/Sieve.NET.Core.Tests/EqualitySieveTests.cs
+++ src/test/Sieve.NET.Core.Tests/EqualitySieveTests.cs
@@ -1,17 +1,95 @@
-﻿using System;
-using System.Linq;
-
+﻿
 namespace Sieve.NET.Core.Tests
 {
+    using System;
+    using System.Linq;
     using System.Collections.Generic;
     using System.ComponentModel;
+    using System.Linq.Expressions;
     using System.Reflection;
     using FluentAssertions;
     using Xunit;
     using Xunit.Extensions;
+    using Xunit.Sdk;
 
     public class EqualitySieveTests
     {
+        public class ToExpressionTests
+        {
+            [Fact]
+            public void SingleValue_MatchesOnlyThatValue()
+            {
+                var aBusinessObjectWithAnIntOf1 = new ABusinessObject { AnInt = 1 };
+                var aBusinessObjectWithAnIntOf2 = new ABusinessObject { AnInt = 2 };
+                var aBusinessObjectWithAnIntOf3 = new ABusinessObject { AnInt = 3 };
+
+                var sieve = new EqualitySieve<ABusinessObject, int>().ForProperty("AnInt").ForValue(1);
+
+                var sut = sieve.ToExpression();
+
+                sut.Compile().Invoke(aBusinessObjectWithAnIntOf1).Should().BeTrue();
+                sut.Compile().Invoke(aBusinessObjectWithAnIntOf2).Should().BeFalse();
+                sut.Compile().Invoke(aBusinessObjectWithAnIntOf3).Should().BeFalse();
+            }
+        }
+
+        public class ToCompiledExpressionTests
+        {
+            [Fact]
+            public void ToCompiledExpression_ReturnsCompiledExpression()
+            {
+                var aBusinessObjectWithAnIntOf1 = new ABusinessObject { AnInt = 1 };
+                var aBusinessObjectWithAnIntOf2 = new ABusinessObject { AnInt = 2 };
+                var aBusinessObjectWithAnIntOf3 = new ABusinessObject { AnInt = 3 };
+
+                var sut = new EqualitySieve<ABusinessObject, int>().ForProperty("AnInt").ForValue(1).ToCompiledExpression();
+
+                sut.Invoke(aBusinessObjectWithAnIntOf1).Should().BeTrue();
+                sut.Invoke(aBusinessObjectWithAnIntOf2).Should().BeFalse();
+                sut.Invoke(aBusinessObjectWithAnIntOf3).Should().BeFalse();
+
+                //TODO:
+            }
+        }
+        public class ImplicitExpressionConversionTests
+        {
+            [Fact]
+            public void SingleValue_ImplicitConversionToFunc_WorksTheSameAsCompiledExpression()
+            {
+                var aBusinessObjectWithAnIntOf1 = new ABusinessObject { AnInt = 1 };
+                var aBusinessObjectWithAnIntOf2 = new ABusinessObject { AnInt = 2 };
+                var aBusinessObjectWithAnIntOf3 = new ABusinessObject { AnInt = 3 };
+
+                //Implicit conversion means there's no call to ToExpression() necessary here.
+
+                Func<ABusinessObject, bool> sut =
+                    new EqualitySieve<ABusinessObject, int>().ForProperty("AnInt").ForValue(1);
+
+                sut.Invoke(aBusinessObjectWithAnIntOf1).Should().BeTrue();
+                sut.Invoke(aBusinessObjectWithAnIntOf2).Should().BeFalse();
+                sut.Invoke(aBusinessObjectWithAnIntOf3).Should().BeFalse();
+
+                //TODO:
+            }
+
+            [Fact]
+            public void SingleValue_ImplicitConversionToExpression_WorksTheSameAsToExpression()
+            {
+                var aBusinessObjectWithAnIntOf1 = new ABusinessObject { AnInt = 1 };
+                var aBusinessObjectWithAnIntOf2 = new ABusinessObject { AnInt = 2 };
+                var aBusinessObjectWithAnIntOf3 = new ABusinessObject { AnInt = 3 };
+
+                //Implicit conversion means there's no call to ToExpression() necessary here.
+
+                Expression<Func<ABusinessObject, bool>> sut =
+                    new EqualitySieve<ABusinessObject, int>().ForProperty("AnInt").ForValue(1);
+
+                sut.Compile().Invoke(aBusinessObjectWithAnIntOf1).Should().BeTrue();
+                sut.Compile().Invoke(aBusinessObjectWithAnIntOf2).Should().BeFalse();
+                sut.Compile().Invoke(aBusinessObjectWithAnIntOf3).Should().BeFalse();
+            }
+            //TODO
+        }
         public class ForPropertyTests
         {
             [Fact]
@@ -150,14 +228,15 @@ namespace Sieve.NET.Core.Tests
             }
         }
 
-        public class ForValues
+        public class ForValuesTests
         {
-            public class SeparatedString
+
+            public class SeparatedStringTests
             {
                 //TODO: Finish
             }
 
-            public class ListOfPropertyType
+            public class IEnumerableOfPropertyTypeTests
             {
                 //TODO: Finish
             }
@@ -208,6 +287,42 @@ namespace Sieve.NET.Core.Tests
             }
         }
 
+        public Expression<Func<TTypeOfObjectToFilter, bool>> ToExpression()
+        {
+            var item = Expression.Parameter(typeof(TTypeOfObjectToFilter), "item");
+            var property = Expression.PropertyOrField(item, PropertyToFilter.Name);
+
+            var acceptableConstants = this.AcceptableValues.Select(acceptableValueItem => Expression.Constant(acceptableValueItem, typeof(TPropertyType))).ToList();
+
+            // take each expression constant and put it into a binary expression of property == constant expression
+            var binaryExpressions = acceptableConstants.Select(constantExpressionItem => Expression.Equal(property, constantExpressionItem)).ToList();
+
+            // for each binary expression, create a list of Expression lambdas 
+            var lambdas = binaryExpressions.Select(binExpression => Expression.Lambda<Func<TTypeOfObjectToFilter, bool>>(binExpression, item));
+
+            var expressionToReturn = PredicateBuilder.False<TTypeOfObjectToFilter>();
+
+            foreach (var lambdaItem in lambdas)
+            {
+                expressionToReturn = expressionToReturn.Or(lambdaItem);
+            }
+
+            return expressionToReturn;
+
+        }
+
+        public static implicit operator Expression<Func<TTypeOfObjectToFilter, bool>>(
+            EqualitySieve<TTypeOfObjectToFilter, TPropertyType> sieve)
+        {
+            return sieve.ToExpression();
+        }
+
+        public static implicit operator Func<TTypeOfObjectToFilter, bool>(
+            EqualitySieve<TTypeOfObjectToFilter, TPropertyType> sieve)
+        {
+            return sieve.ToCompiledExpression();
+        }
+
 
         private static void EnsurePropertyTypesMatch(PropertyInfo matchingProperty)
         {
@@ -252,6 +367,9 @@ namespace Sieve.NET.Core.Tests
         //    throw new NotImplementedException();
         //}
 
-
+        public Func<TTypeOfObjectToFilter, bool> ToCompiledExpression()
+        {
+            return this.ToExpression().Compile();
+        }
     }
 }
