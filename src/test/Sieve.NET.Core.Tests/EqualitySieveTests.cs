bcbee7a73e09b7ecb3c0e32571686e0770b69953
Sean Killeen <SeanKilleen@gmail.com>
2014-05-30 16:59:06 -0400

    ForAdditionalValues() adds to existing list

--- src/test/Sieve.NET.Core.Tests/EqualitySieveTests.cs
+++ src/test/Sieve.NET.Core.Tests/EqualitySieveTests.cs
@@ -5,6 +5,7 @@ namespace Sieve.NET.Core.Tests
     using System.Collections.Generic;
     using System.Diagnostics;
     using System.Globalization;
+    using System.Linq;
     using System.Linq.Expressions;
 
     using FluentAssertions;
@@ -393,7 +394,17 @@ namespace Sieve.NET.Core.Tests
                 [Fact]
                 public void WhenValuesExist_AddsAdditionalValues()
                 {
-                    throw new NotImplementedException();
+                    var listOfValues = new List<int> { 1, 2, 3 };
+                    var additionalValues = new List<int> { 4, 5, 6 };
+
+                    var fullList = listOfValues.ToList().Union(additionalValues);
+                    
+                    var sut = new EqualitySieve<ABusinessObject>()
+                        .ForProperty(x => x.AnInt)
+                        .ForValues(listOfValues)
+                        .ForAdditionalValues(additionalValues);
+
+                    sut.AcceptableValues.Should().BeEquivalentTo(fullList);
                 }
             }
 
