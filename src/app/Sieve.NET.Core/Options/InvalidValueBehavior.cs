5299bac0188f57d6e45ae03ec7896568c6410091
Sean Killeen <SeanKilleen@gmail.com>
2014-06-02 12:05:23 -0400

    Documented behavior options.

--- src/app/Sieve.NET.Core/Options/InvalidValueBehavior.cs
+++ src/app/Sieve.NET.Core/Options/InvalidValueBehavior.cs
@@ -1,5 +1,9 @@
 namespace Sieve.NET.Core.Options
 {
+    /// <summary>
+    /// A behavior to use when the Sieve encounters an invalid acceptable value.
+    /// </summary>
+    /// <remarks>The default, 0, is to ignore the invalid value.</remarks>
     public enum InvalidValueBehavior
     {
         IgnoreInvalidValue = 0, //default
