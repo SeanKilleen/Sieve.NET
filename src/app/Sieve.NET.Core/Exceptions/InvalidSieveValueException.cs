596849dde1dd9f6d2b824fb1cb5eca439a4d1c22
Sean Killeen <SeanKilleen@gmail.com>
2014-06-02 12:03:38 -0400

    Documented Exceptions

--- src/app/Sieve.NET.Core/Exceptions/InvalidSieveValueException.cs
+++ src/app/Sieve.NET.Core/Exceptions/InvalidSieveValueException.cs
@@ -6,6 +6,9 @@ using System.Threading.Tasks;
 
 namespace Sieve.NET.Core.Exceptions
 {
+    /// <summary>
+    /// Indicates that a value supplied to a Sieve as an acceptable value is actually invalid.
+    /// </summary>
     public class InvalidSieveValueException : Exception
     {
         public InvalidSieveValueException(string message)
--- src/app/Sieve.NET.Core/Exceptions/NoSieveValuesSuppliedException.cs
+++ src/app/Sieve.NET.Core/Exceptions/NoSieveValuesSuppliedException.cs
@@ -2,6 +2,10 @@ namespace Sieve.NET.Core.Exceptions
 {
     using System;
 
+    
+    /// <summary>
+    /// Indicates that a Sieve's list of acceptable values is empty.
+    /// </summary>
     public class NoSieveValuesSuppliedException : Exception
     {
     }
